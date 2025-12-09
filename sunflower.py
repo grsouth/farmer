9267016386cd905b8e1e62a053a26713efffbb32
Garrett Southam <garrettsoutham@proton.me>
2025-12-09 12:31:15 -0700

    sunflower draft

--- sunflower.py
+++ sunflower.py
@@ -1,4 +1,43 @@
 def plant_twobysix():
-  for i in range(2):
-    for j in range(6):
-      if get
+
+  for i in range(6):
+    if get_ground_type() != Grounds.Soil:
+      till()
+    plant(Entities.Sunflower)
+    if i < 5:
+      move(North)
+  
+  move(East)
+
+  for i in range(6):
+    if get_ground_type() != Grounds.Soil:
+      till()
+    plant(Entities.Sunflower)
+    if i < 5:
+      move(South)
+
+def measure_twobysix():
+
+  biggest_sunflower = (-1, -1, -1)
+
+  for i in range(6):
+
+    size = measure()
+    if size > biggest_sunflower[0]:
+      biggest_sunflower = (size, get_pos_x(), get_pos_y())
+      
+    if i < 5:
+      move(North)
+  
+  move(East)
+
+  for i in range(6):
+    sunflowers.append((measure(), get_pos_x(), get_pos_y()))
+    if i < 5:
+      move(South)
+
+  return sunflowers
+
+def harvest_biggest_sunflower(sunflowers):
+  
+
